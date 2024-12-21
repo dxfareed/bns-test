b24b8e5073499219fcefb86d19d5991497e6e856
dxfareed <einstein1101@proton.me>
2024-12-21 22:02:23 +0100

    refact

--- app/meme/i/route.tsx
+++ app/meme/i/route.tsx
@@ -6,6 +6,7 @@ export const runtime = 'edge';
 
 export async function GET(request : Request){
     const {searchParams} = new URL(request.url);
+    
     let listNames;
     let ftname;
     let len;
